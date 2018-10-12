85d2d1df71eadc0149bd7180c902108537c5d289
Mustafa Iman <mustafaiman@gmail.com>
2018-10-12 09:22:07 -0400

    update

--- run-tests.sh
+++ run-tests.sh
@@ -4,10 +4,35 @@ MEMBERS=2
 CLIENTS=1
 DURATION=2m
 
-$SIMULATOR_HOME/bin/coordinator cache.no.expiration.properties --members $MEMBERS --clients $CLIENTS --duration $DURATION --sessionId m${MEMBERS}-c${CLIENTS}-cache-no-expiration
-$SIMULATOR_HOME/bin/coordinator cache.expiration.properties --members $MEMBERS --clients $CLIENTS --duration $DURATION --sessionId m${MEMBERS}-c${CLIENTS}-cache-expiration
-$SIMULATOR_HOME/bin/coordinator cache.expiration.properties --members $MEMBERS --clients $CLIENTS --duration $DURATION --sessionId m${MEMBERS}-c${CLIENTS}-cache-expiration-delay-task --memberArgs -Dhazelcast.internal.cache.expiration.task.period.seconds=300
-$SIMULATOR_HOME/bin/coordinator cache.no.expiration.properties --members $MEMBERS --clients $CLIENTS --duration $DURATION --sessionId m${MEMBERS}-c${CLIENTS}-cache-no-expiration-100 --memberArgs -Dhazelcast.internal.cache.expiration.cleanup.percentage=100
-$SIMULATOR_HOME/bin/coordinator cache.expiration.properties --members $MEMBERS --clients $CLIENTS --duration $DURATION --sessionId m${MEMBERS}-c${CLIENTS}-cache-expiration-100 --memberArgs -Dhazelcast.internal.cache.expiration.cleanup.percentage=100
-$SIMULATOR_HOME/bin/coordinator cache.no.expiration.hotkey.properties --members $MEMBERS --clients $CLIENTS --duration $DURATION --sessionId m${MEMBERS}-c${CLIENTS}-cache-hotkey-no-expiration
-$SIMULATOR_HOME/bin/coordinator cache.expiration.hotkey.properties --members $MEMBERS --clients $CLIENTS --duration $DURATION --sessionId m${MEMBERS}-c${CLIENTS}-hotkey-cache-expiration
+memberHeapSZ=1G
+clientHeapSZ=512M
+
+partitions=271
+monitorSec=30
+
+gcArgs="-verbose:gc -Xloggc:verbosegc.log"
+gcArgs="${gcArgs} -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -XX:+PrintTenuringDistribution -XX:+PrintGCApplicationStoppedTime -XX:+PrintGCApplicationConcurrentTime"
+
+memberJvmArgs="-Dhazelcast.partition.count=${partitions}"
+memberJvmArgs="${memberJvmArgs} -Dhazelcast.health.monitoring.level=NOISY -Dhazelcast.health.monitoring.delay.seconds=${monitorSec}"
+memberJvmArgs="${memberJvmArgs} -Xmx${memberHeapSZ} -XX:+HeapDumpOnOutOfMemoryError"
+memberJvmArgs="${memberJvmArgs} ${gcArgs}"
+
+clientJvmArgs="-Xmx${clientHeapSZ} -XX:+HeapDumpOnOutOfMemoryError"
+clientJvmArgs="${clientJvmArgs} ${gcArgs}"
+
+run 2 1 2m cache.no.expiration.properties
+$SIMULATOR_HOME/bin/coordinator cache.no.expiration.properties --members $MEMBERS --clients $CLIENTS --duration $DURATION --sessionId m${MEMBERS}-c${CLIENTS}-cache-no-expiration --memberArgs "${memberArgs}" --clientArgs "${clientArgs}"
+$SIMULATOR_HOME/bin/coordinator --clean
+$SIMULATOR_HOME/bin/coordinator cache.expiration.properties --members $MEMBERS --clients $CLIENTS --duration $DURATION --sessionId m${MEMBERS}-c${CLIENTS}-cache-expiration --memberArgs "${memberArgs}" --clientArgs "${clientArgs}"
+$SIMULATOR_HOME/bin/coordinator --clean
+$SIMULATOR_HOME/bin/coordinator cache.expiration.properties --members $MEMBERS --clients $CLIENTS --duration $DURATION --sessionId m${MEMBERS}-c${CLIENTS}-cache-expiration-delay-task --memberArgs "-Dhazelcast.internal.cache.expiration.task.period.seconds=300 ${memberArgs}" --clientArgs "${clientArgs}"
+$SIMULATOR_HOME/bin/coordinator --clean
+$SIMULATOR_HOME/bin/coordinator cache.no.expiration.properties --members $MEMBERS --clients $CLIENTS --duration $DURATION --sessionId m${MEMBERS}-c${CLIENTS}-cache-no-expiration-100 --memberArgs "-Dhazelcast.internal.cache.expiration.cleanup.percentage=100 ${memberArgs}" --clientArgs "${clientArgs}"
+$SIMULATOR_HOME/bin/coordinator --clean
+$SIMULATOR_HOME/bin/coordinator cache.expiration.properties --members $MEMBERS --clients $CLIENTS --duration $DURATION --sessionId m${MEMBERS}-c${CLIENTS}-cache-expiration-100 --memberArgs "-Dhazelcast.internal.cache.expiration.cleanup.percentage=100 ${memberArgs}" --clientArgs "${clientArgs}"
+$SIMULATOR_HOME/bin/coordinator --clean
+$SIMULATOR_HOME/bin/coordinator cache.no.expiration.hotkey.properties --members $MEMBERS --clients $CLIENTS --duration $DURATION --sessionId m${MEMBERS}-c${CLIENTS}-cache-hotkey-no-expiration --memberArgs "${memberArgs}" --clientArgs "${clientArgs}"
+$SIMULATOR_HOME/bin/coordinator --clean
+$SIMULATOR_HOME/bin/coordinator cache.expiration.hotkey.properties --members $MEMBERS --clients $CLIENTS --duration $DURATION --sessionId m${MEMBERS}-c${CLIENTS}-hotkey-cache-expiration --memberArgs "${memberArgs}" --clientArgs "${clientArgs}"
+$SIMULATOR_HOME/bin/coordinator --clean
